c8c30585737f3a8101dc7e33991b94f324bb5731
Sirtalyon <36968485+Sirtalyon@users.noreply.github.com>
2019-07-25 23:00:34 +0200

    Mise en place du Navmesh
    J'ai mis en place le navmesh, faudra le peaufiné mais c'est pas mal

---
+++ Assets/MoveTo.cs
@@ -0,0 +1,28 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    public AnimationClip run;
+    public AnimationClip idle;
+
+    void Update()
+    {
+        GetComponent<Animation>().Play(run.name);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
