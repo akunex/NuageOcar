a800306af3094ef8c3abc1e5f0b1e2973736bf01
akunex <akunex@live.fr>
2019-07-26 04:55:46 +0200

    Ajout des animations et paramétrisation du déplacement
    J'ai appliqué les bons paramètres au navmeshagent pour qu'il se déplace proprement. J'ai aussi ajouter les animations de déplacement. Il y a un bug c'est quand on clique sur un mur il veut aller dans le mur

--- Assets/MoveTo.cs
+++ Assets/MoveTo.cs
@@ -7,22 +7,48 @@ public class MoveTo : MonoBehaviour
 
     public Camera cam;
     public NavMeshAgent agent;
+    private Vector3 targetPosition;
+
+    private bool isMoving;
 
     public AnimationClip run;
     public AnimationClip idle;
 
+    void Start()
+    {
+        isMoving = false;
+    }
+
     void Update()
     {
-        GetComponent<Animation>().Play(run.name);
+        
         if (Input.GetMouseButtonDown(0))
         {
+            isMoving = true;
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if(Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit))
             {
                 agent.SetDestination(hit.point);
+                targetPosition = hit.point;
             }
+            
+        }
+        // Faut trouver une meilleur condition que ça
+        Debug.Log(Vector3.Distance(transform.position, targetPosition));
+        Debug.DrawLine(transform.position, targetPosition, Color.red);
+        if (Vector3.Distance(transform.position, targetPosition) < 1)
+        {
+            isMoving = false;
+        }
+        if (isMoving)
+        {
+            GetComponent<Animation>().Play(run.name);
+        }
+        else
+        {
+            GetComponent<Animation>().Play(idle.name);
         }
     }
 }
