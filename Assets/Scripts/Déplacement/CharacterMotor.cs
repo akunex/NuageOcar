39bf02e4169dbf08773dde11098d59055a97f11a
akunex <akunex@live.fr>
2019-07-26 06:37:12 +0200

    Ajout PathPreview (c chelou) + prefabs du squelette
    J'ai mis un algo qui affiche le chemin, faut le travailler pour faire un système de preview. Je l'ai trouvé sur le net je comprend pas comment il marche. J'ai mis le squelette dans la prefabs comme sa si on le del de la scene on peut le remettre easy

---
+++ Assets/Scripts/Déplacement/CharacterMotor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterMotor : MonoBehaviour
+{
+
+    public Camera cam;
+    public NavMeshAgent agent;
+    private Vector3 targetPosition;
+
+    private bool isMoving;
+
+    public AnimationClip run;
+    public AnimationClip idle;
+
+    void Start()
+    {
+        isMoving = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMoving = true;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+                targetPosition = hit.point;
+            }
+            
+        }
+        // Il y a peut être une meilleur solution ?
+        Debug.Log(Vector3.Distance(transform.position, targetPosition));
+        Debug.DrawLine(transform.position, targetPosition, Color.red);
+        if (Vector3.Distance(transform.position, targetPosition) < 1)
+        {
+            isMoving = false;
+        }
+        if (isMoving)
+        {
+            this.GetComponent<Animation>().Play("run");
+        }
+        else
+        {
+            this.GetComponent<Animation>().Play("idle");
+        }
+    }
+}
