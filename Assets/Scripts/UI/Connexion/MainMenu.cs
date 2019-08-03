e30443c5054cf14a1322e7ebd617ca28d817e5d2
akunex <akunex@live.fr>
2019-08-03 06:20:55 +0200

    Page de connexion 1/2
    Connexion + sounds
    Il faut faire les options mtn

---
+++ Assets/Scripts/UI/Connexion/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public InputField pseudo;
+    public InputField password;
+    public Text error;
+
+    void Start()
+    {
+        error.enabled = false;
+    }
+    public void PlayGame()
+    {
+        if (pseudo.text == "Beta" && password.text == "test")
+        {
+            SceneManager.LoadScene("MainScene");
+            error.enabled = false;
+        }
+        else
+        {
+            error.enabled = true;
+        }
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/Connexion/MusicButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MusicButton : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioSource mouseOverMusic;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouseOverMusic.Play();
+        Debug.Log("test");
+    }
+}
