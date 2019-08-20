da9c585f13c124f84d14dc80f879d2d9c1196fb9
akunex <akunex@live.fr>
2019-08-20 18:53:30 +0200

    Création du menu IG et du Frame état personnage

---
+++ Assets/Assets/Ocar_GUI/Menu_opening.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_opening : MonoBehaviour
+{
+    public GameObject panel;
+    public void OpenPanel()
+    {
+        Animator animator = panel.GetComponent<Animator>();
+        if(animator != null)
+        {
+            bool isOpen = animator.GetBool("open");
+            animator.SetBool("open", !isOpen);
+        }
+    }
+}
