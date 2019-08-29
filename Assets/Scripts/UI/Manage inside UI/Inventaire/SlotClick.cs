900719f0bd8ead784df870e564c29e2379bb264a
akunex <akunex@live.fr>
2019-08-29 12:12:12 +0200

    Update inventorypopup

--- Assets/Scripts/UI/Manage inside UI/Inventaire/SlotClick.cs
+++ Assets/Scripts/UI/Manage inside UI/Inventaire/SlotClick.cs
@@ -9,18 +9,20 @@ public class SlotClick : MonoBehaviour, IPointerClickHandler
     public Canvas parentCanvas;
     Vector3 Offset = Vector3.zero;
 
+    public GameObject[] inventoryPopup;
+
     void Start()
     {
         parentCanvas = GetComponentInParent<Canvas>();
         //Demi longueur de la popup
-        Offset.Set(50, -15, 0);
+        Offset.Set(-50, -15, 0);
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Left)
         {
-
+            ClosePopup();
         }
         else if (eventData.button == PointerEventData.InputButton.Middle)
         {
@@ -29,11 +31,28 @@ public class SlotClick : MonoBehaviour, IPointerClickHandler
         //Affichage popup et move du cadre sur le pointeur
         else if (eventData.button == PointerEventData.InputButton.Right)
         {
+            ClosePopup();
             popup.SetActive(true);
             Vector2 movePos;
             RectTransformUtility.ScreenPointToLocalPointInRectangle(parentCanvas.transform as RectTransform, eventData.position, parentCanvas.worldCamera, out movePos);
             popup.transform.position = parentCanvas.transform.TransformPoint(movePos) + Offset;
         }
     }
+
+    public void ClosePopup()
+    {
+        inventoryPopup = GameObject.FindGameObjectsWithTag("InventorySlotPopup");
+        foreach (GameObject popup in inventoryPopup)
+        {
+            popup.SetActive(false);
+        }
+        Debug.Log("click");
+    }
+    
+    public void Test()
+    {
+        Debug.Log("input pressed");
+    }
+    
 }
 
---
+++ Assets/Scripts/UI/UImanager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UImanager : MonoBehaviour
+{
+   public GameObject[] inventoryPopup;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !IsOverInventoryPopup() || Input.GetMouseButtonDown(1) && !IsOverInventoryPopup())
+        {
+            inventoryPopup = GameObject.FindGameObjectsWithTag("InventorySlotPopup");
+            foreach (GameObject popup in inventoryPopup)
+            {
+                popup.SetActive(false);
+            }
+        }      
+    }
+
+    public bool IsOverInventoryPopup()
+    {
+
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            PointerEventData pointerData = new PointerEventData(EventSystem.current)
+            {
+                pointerId = -1,
+            };
+
+            pointerData.position = Input.mousePosition;
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerData, results);
+
+            if (results.Count > 0)
+            {
+                for (int i = 0; i < results.Count; ++i)
+                {
+                    if (results[i].gameObject.CompareTag("InventorySlotPopup"))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+}
