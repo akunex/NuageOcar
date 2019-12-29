d3ca724f605c2d8730e8562c4a9b8eff3a02baac
akunex <akunex@live.fr>
2019-12-29 19:42:44 +0100

    wtf

---
+++ Assets/Scripts/Battle/SpellOne.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellOne : MonoBehaviour
+{
+
+    public Rigidbody ball;
+    public Transform target;
+    public GameObject targetGO;
+    public GameObject test;
+    public Material mat;
+    public float h = 25;
+    public float gravity = -18;
+
+    public bool debugPath;
+
+    public LayerMask movementMask;
+
+    void Start()
+    {
+        ball.useGravity = false;
+        target = null;
+        test.name = "bite";
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Launch();
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, movementMask))
+        {
+            targetGO = Instantiate(new GameObject(), hit.point, Quaternion.identity);
+            target = targetGO.transform;
+            Destroy(targetGO);
+        }
+
+        if (debugPath)
+        {
+            DrawPath();
+        }
+    }
+
+    void Launch()
+    {
+        Physics.gravity = Vector3.up * gravity;
+        ball.useGravity = true;
+        ball.velocity = CalculateLaunchData().initialVelocity;
+    }
+
+    LaunchData CalculateLaunchData()
+    {
+        float displacementY = target.position.y - ball.position.y;
+        Vector3 displacementXZ = new Vector3(target.position.x - ball.position.x, 0, target.position.z - ball.position.z);
+        float time = Mathf.Sqrt(-2 * h / gravity) + Mathf.Sqrt(2 * (displacementY - h) / gravity);
+        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * h);
+        Vector3 velocityXZ = displacementXZ / time;
+
+        return new LaunchData(velocityXZ + velocityY * -Mathf.Sign(gravity), time);
+    }
+
+    void DrawPath()
+    {
+        LaunchData launchData = CalculateLaunchData();
+        Vector3 previousDrawPoint = ball.position;
+
+        int resolution = 30;
+        for (int i = 1; i <= resolution; i++)
+        {
+            float simulationTime = i / (float)resolution * launchData.timeToTarget;
+            Vector3 displacement = launchData.initialVelocity * simulationTime + Vector3.up * gravity * simulationTime * simulationTime / 2f;
+            Vector3 drawPoint = ball.position + displacement;
+            DrawLine(previousDrawPoint, drawPoint, Color.green, 0.1f);
+            
+            //Debug.DrawLine(previousDrawPoint, drawPoint, Color.green);
+            previousDrawPoint = drawPoint;
+        }
+    }
+
+
+
+    void DrawLine(Vector3 start, Vector3 end, Color color, float duration)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.material = mat;
+        lr.startColor = color;
+        lr.endColor = color;
+        lr.SetWidth(5f, 5f);
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        GameObject.Destroy(myLine, duration);
+    }
+
+
+
+    struct LaunchData
+    {
+        public readonly Vector3 initialVelocity;
+        public readonly float timeToTarget;
+
+        public LaunchData(Vector3 initialVelocity, float timeToTarget)
+        {
+            this.initialVelocity = initialVelocity;
+            this.timeToTarget = timeToTarget;
+        }
+
+    }
+}
